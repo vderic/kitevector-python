3ae107333679b0dedb3015cd50545b702b15e151
eric <ericlam@vitessedata.com>
2024-01-02 17:26:14 +0000

    add parquet filespec

--- test/test.py
+++ test/test.py
@@ -10,10 +10,12 @@ from kitevector import vector
 if __name__ == "__main__":
 
 	path = 'tmp/vector/vector*.csv'
-	filespec = kite.CsvFileSpec()
+	csvspec = kite.CsvFileSpec()
+	parquetspec = kite.ParquetFileSpec()
+
 	hosts = ['localhost:7878']
 
-	vs = vector.KiteVector(hosts, path, filespec)
+	vs = vector.KiteVector(hosts, path, csvspec)
 
 	res = vs.nbest([4,6,8], -70, 3)
 	print(res)
