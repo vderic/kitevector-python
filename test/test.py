9bbd7075b54d1bdc87dc796480fdcac246d4a8af
eric <ericlam@vitessedata.com>
2024-01-04 12:00:46 +0000

    add filter

--- test/test.py
+++ test/test.py
@@ -24,6 +24,8 @@ def gen_embedding(nitem):
 
 if __name__ == "__main__":
 
+	random.seed(1)
+
 	path = 'tmp/vector/vector*.parquet'
 	#csvspec = kite.CsvFileSpec()
 	parquetspec = kite.ParquetFileSpec()
@@ -45,19 +47,10 @@ if __name__ == "__main__":
 		print(ids)
 		print(scores)
 
-		#ids = [1,2]
-		#vs = vector.KiteVector(hosts, path, parquetspec, fragcnt)
-		#res = vs.inner_product(embedding, threshold, nbest, ids=ids)
-		#print(res)
-
-		#docids = [10,20]
-		#vs = vector.KiteVector(hosts, path, parquetspec, fragcnt)
-		#res = vs.inner_product(embedding, threshold, nbest, docids=docids)
-		#print(res)
-
-		#filter = 'id IN (1,3)'
-		#vs = vector.KiteVector(hosts, path, parquetspec, fragcnt)
-		#res = vs.inner_product(embedding, threshold, nbest, filter=filter)
-		#print(res)
+		filter = 'id IN (999, 4833)'
+		vs = vector.KiteVector(schema, hosts, path, parquetspec, fragcnt)
+		ids, scores = vs.inner_product(embedding, id_cname, threshold, nbest, filter=filter)
+		print(ids)
+		print(scores)
 	except Exception as msg:
 		print(msg)
