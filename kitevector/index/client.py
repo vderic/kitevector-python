090e549e8a9e300898e47ceb7b15d0131bca5c96
eric <ericlam@vitessedata.com>
2024-01-08 10:57:26 +0000

    index

---
+++ kitevector/index/client.py
@@ -0,0 +1,103 @@
+import http.client
+import json
+import selectors
+
+class KiteIndexClient:
+	
+	def __init__(self):
+		self.selectors = selectors.DefaultSelector()
+		self.connections = []
+		self.responses = []
+		self.batches = []
+
+
+	def query(self):
+
+		for i in range(2):
+			
+			conn = http.client.HTTPConnection('localhost', 8181)
+
+			headers = {'Content-type': 'application/json'}
+
+			foo = {'text': 'Hello HTTP #1 **cool**, and #1!'}
+			json_data = json.dumps(foo)
+
+			conn.request('POST', '/query', json_data, headers)
+
+			self.connections.append(conn)
+
+		for c in self.connections:
+			r = c.getresponse()
+			self.responses.append(r)
+			self.selectors.register(r, selectors.EVENT_READ, self.read)
+
+
+	def build_index(self):
+		pass
+
+	def delete_index(self):
+		pass
+
+	def read(self, response, mask):
+		try:
+			print(response.fileno())
+			return response.read().decode()
+		except Exception as msg:
+			print(msg)
+			raise
+
+		return None
+
+	def next(self):
+
+		if len(self.batches) != 0:
+			return self.batches.pop()
+
+		while True:
+			if len(self.responses) == 0:
+				break
+
+			events = self.selectors.select(None)
+
+			for key, mask in events:
+				callback = key.data
+				res = callback(key.fileobj, mask)
+				self.batches.append(res)
+				self.selectors.unregister(key.fileobj)
+				try:
+					key.fileobj.close()
+				except OSError as msg:
+					print(msg)
+
+				self.responses.remove(key.fileobj)
+			
+			if len(self.batches) > 0:
+				return self.batches.pop()
+
+		if len(self.batches) == 0:
+			return None
+
+		return self.batches.pop()
+
+	def close(self):
+		for c in self.connections:
+			c.close()
+
+		self.selectors.close()
+				
+
+if __name__ == "__main__":
+	
+	client = KiteIndexClient()
+	try:
+		client.query()
+		while True:
+			r = client.next()
+			if r is None:
+				break
+			print(r)
+	except Exception as msg:
+		print(msg)
+	finally:
+		client.close()
+
---
+++ kitevector/index/server.py
@@ -0,0 +1,52 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from urllib.parse import urlparse
+import json
+
+class KiteIndex:
+	indexes = {}
+
+class RequestHandler(BaseHTTPRequestHandler):
+
+	def build_index(self):
+		pass
+
+	def query(self):
+		print("query")
+		embedding = [12,2,3,4]
+		msg = json.dumps(embedding).encode('utf-8')
+		
+		self.send_response(200)
+		self.send_header("Content-Length", len(msg))
+		self.send_header("Content-Type", "application/json")
+		self.end_headers()
+
+		self.wfile.write(msg)
+
+
+	
+	def do_DELETE(self):
+		if self.path == 'delete_index':
+			self.delete_index()
+
+	def do_POST(self):
+		content_length = int(self.headers.get("Content-Length"))
+		body = self.rfile.read(content_length)
+
+		print("path = " , self.path)
+
+		if self.path == '/build_index':
+			self.build_index()
+		elif self.path == '/query':
+			self.query()
+		else:
+			pass
+
+
+
+def run():
+	server = ('', 8181)
+	httpd = HTTPServer(server, RequestHandler)
+	httpd.serve_forever()
+
+if __name__ == "__main__":
+	run()
