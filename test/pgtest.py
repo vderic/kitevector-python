c7d9c2631e0534bc9a7cacca7a6cef98bfb4cff1
eric <ericlam@vitessedata.com>
2024-01-11 13:52:29 +0000

    update pgtest

--- test/pgtest.py
+++ test/pgtest.py
@@ -3,7 +3,6 @@ import random
 import math
 from kite import kite
 from kite.xrg import xrg
-
 from kitevector import vector as kv
 
 def gen_embedding(nitem):
@@ -24,17 +23,21 @@ if __name__ == "__main__":
 	random.seed(1)
 	path = 'ext_ai'
 
-	# generate SQL for Postgres: SELECT id, docid from table WHERE embedding <#> '[...]' ORDER BY embedding <#> '[...]' LIMIT 5
+	# index specific setting
+	index_params = {
+		"metric_type": "l2",
+		"index_type": "hnsw",
+		"params":{
+			"id_field" : "id",
+			"embedding_field": "embedding"
+		}
+	}
+
 	vs = kv.PgVector()
 	embed = gen_embedding(1536)
-	vs.select(['id', 'docid']).table(path).order_by(kv.VectorExpr("embedding").inner_product(embed))
-	vs.filter(kv.OpExpr('>', kv.VectorExpr("embedding").inner_product(embed), 0.07)).limit(5)
-	sql = vs.sql()
-	print(sql)
 
-	# genarte SQL for Postgres:  SELECT id, docid from path WHERE id IN (1,2,3) AND id > 0.7 ORDER BY docid LIMIT 6
 	vs1 = kv.PgVector()
-	vs1.select([kv.Var('id'), kv.OpExpr('+', 'docid', 2)]).table(path).order_by(kv.Var("docid"))
+	vs1.select([kv.Var('id'), kv.OpExpr('-', 'docid', 6)]).table(path).index(index_params, embed)
 	vs1.filter(kv.ScalarArrayOpExpr("id", [1,2,3])).filter(kv.OpExpr('>', 'id', 0.7)).limit(5)
 	sql = vs1.sql()
 	print(sql)
